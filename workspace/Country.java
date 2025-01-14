31885b0316581a568cad738e3df2e6f43d8f4604
XDagging <113935822+XDagging@users.noreply.github.com>
2025-01-14 19:07:21 +0000

    askdjakjsdf

--- workspace/Country.java
+++ workspace/Country.java
@@ -2,6 +2,51 @@ public class Country
 {
   // add private instance variables for the name, capital, language, and image file.
 
+  private String name="";
+  private String capital="";
+  private String language="";
+  private String imageFile="";
+
+
+  public Country(String name, String capital, String language, String imageFile) 
+  {
+    this.name = name;
+    this.capital = capital;
+    this.language = language;
+    this.imageFile = imageFile;
+  }
+
+  public Country() {
+    name = "United States";
+    capital = "Washington D.C.";
+    language = "English";
+    imageFile = "US.jpg";
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public String getCapital() {
+    return capital;
+  }
+
+  public String getLanguage() {
+    return language;
+  }
+
+  public String getImageFile() {
+    return imageFile;
+  }
+
+  public String toString() {
+
+    return name+"'s capital is " + capital + " and its primary language is " + language;
+  }
+
+
+
   // add constructors
 
   // Write accessor/get methods for each instance variable that returns it.
@@ -10,5 +55,7 @@ public class Country
  
 
 
+
+  // getImage()
   
 }
--- workspace/Main.java
+++ workspace/Main.java
@@ -17,7 +17,13 @@ public class Main
   private ImageIcon img;
   private JLabel imageLabel;
   private JLabel outputLabel;
+  private JTextField textField;
+
+  private JTextField input;
   
+
+    // Precondition: initializes the gui
+  // Postcondition: initializes the post condition
   public static void main(String[] args) {
     // Create the GUI
     Main gui = new Main();
@@ -25,6 +31,10 @@ public class Main
     gui.showCountry();
   }
 
+
+
+  // Precondition: Country object has an img file
+  // Postcondition: creates a Country class for each country inside the csv file. 
   /* loadCountries() reads in the data from the countries-data.csv file and fills in the countryArray with data. You need to add the loop that reads in the country data into the array. */
   public void loadCountries() 
   {
@@ -40,9 +50,13 @@ public class Main
     // Write a for loop that goes through the countryArray.
     // for(int i ....) {
     // Do the following inside the loop
+    for (int i=0; i<countryArray.length; i++) {
       String input = scan.nextLine();
       String[] data = input.split(",");
       System.out.println("Read in " + data[0]);
+      countryArray[i] = new Country(data[0],data[1],data[2],data[3]);
+    }
+      
       // inside the loop, create a new Country using your constructor with 4 arguments and pass in data[0], data[1], data[2], data[3] as arguments.
      // inside the loop, set countryArray[i] to the created Country object
      
@@ -51,33 +65,101 @@ public class Main
 
   /* showCountry() will show the image associated with the current country. It should get the country at index from the countryArray. It should use its get method to get its image file name and use the code below to put the image in the GUI.
   */
+
+
+
+    // Precondition: Index is defined
+  // Postcondition:  Sets the image of the current country
   public void showCountry() {
     // Get the country at index from countryArray
+    Country currentCountry = countryArray[index];
     
     // Use its get method to get the its image file name and save it into imagefile variable below instead of worldmap.jpg.
-    String imagefile = "worldmap.jpg";
+    String imagefile = currentCountry.getImageFile();
     // Use the following code to create an new Image Icon and put it into the GUI
+    System.out.println(imagefile);
     img = new ImageIcon("/workspaces/Countries/workspace/"+imagefile);
     imageLabel.setIcon(img);
   }
   
   /* nextButton should increment index. If the index is greater than 9, reset it back to 0. Clear the outputLabel to empty string using setText, and call showCountry();*/
+  
+  // Precondition: Index is defined
+  // Postcondition: changes the index and adds the queston to the outputLabel and then calls the showCountry method. 
   public void nextButtonClick()
   {
+    if (index > 9) {
+      index = 0;
+    } else {
+      index++;
+    }
+
+    Country currentCountry = countryArray[index];
+    int randomNum = (int) Math.random()*3;
+    if (randomNum == 0) {
+      outputLabel.setText("What country is this?");
+    } else if (randomNum == 1) {
+      outputLabel.setText("What is this nation's capital?");
+    }
+ 
+    textField.setText("");
+    
+    showCountry();
     
   }
   
   /* reviewButton should get the country at index from the countryArray, call its toString() method and save the result, print it out with System.out.println and as an argument to outputLabel.setText( text to print out ); */
+  
+  
   public void reviewButtonClick()
   {
-     
+    outputLabel.setText(countryArray[index].toString());
+    System.out.println(countryArray[index].toString());  
   }
 
   /* quizButton should clear the outputLabel (outputLabel.setText to empty string), get the country at index from countryArray, print out a question about it like What country is this? and/or What's this country's capital?. Get the user's answer using scan.nextLine() and check if it is equal to the country's data using its get methods and print out correct or incorrect.
   */
   public void quizButtonClick()
   {
-    Scanner scan = new Scanner(System.in); 
+    // Scanner scan = new Scanner(System.in); 
+    // outputLabel.setText("")
+    Country currentCountry = countryArray[index];
+
+    if (outputLabel.getText().indexOf("capital") > 0) {
+
+      // This means that we need to check if it has the same capital
+      if (textField.getText().toLowerCase().equals(currentCountry.getCapital().toLowerCase())) {
+        outputLabel.setText("Good Job! Click Next to continue");
+
+      } else {
+        outputLabel.setText("Incorrect try again!");
+      }
+
+
+
+
+    } else {
+
+      if (textField.getText().toLowerCase().equals(currentCountry.getName().toLowerCase())) {
+        outputLabel.setText("Good Job! Click Next to continue");
+
+      } else {
+        outputLabel.setText("Incorrect try again!");
+      }
+
+
+
+    
+    }
+
+
+    
+   
+    
+
+    // textField.setText("");
+
+
     
     
     
@@ -96,6 +178,9 @@ public Main() {
         JButton reviewButton = new JButton("Review");
         JButton quizButton = new JButton("Quiz");
         JButton newButton = new JButton("Next");
+        
+
+
         jFrame.add(reviewButton);
         jFrame.add(quizButton);
         jFrame.add(newButton);
@@ -104,10 +189,20 @@ public Main() {
         img = new ImageIcon("worldmap.jpg");
         // create a label to display image
         imageLabel = new JLabel(img);
+
         // and one for output
         outputLabel = new JLabel();
         jFrame.add(imageLabel);
+
+        outputLabel.setText("Click next to start");
         jFrame.add(outputLabel);
+
+        // add input
+
+        textField = new JTextField(20);
+
+
+        jFrame.add(textField);
         jFrame.setVisible(true);
         // add event listener for button click
         reviewButton.addActionListener(new ActionListener() {
